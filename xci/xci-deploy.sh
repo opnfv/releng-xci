6f04b5c1496b8fede0a33ccde8cb57258bba88ac
pperiyasamy <periyasamy.palanisamy@ericsson.com>
2017-08-14 14:52:14 +0530

    [WIP] opendaylight integration with xci
    * added ansible-opendaylight repo in a-r-r file
    * added user_variables.yml files for installing ODL and OVS (borrowed it from https://github.com/jvidalallende/releng/blob/rc5/prototypes/xci/file/mini/user_variables.yml)
    * increased VM memory footprint to accomodate ODL process
    * added XCI default deployment option with nosdn
    
    Depends on:
          https://review.openstack.org/#/c/480131/
          https://review.openstack.org/#/c/480128/
    
    Change-Id: I1355f4beb061caab7315b6f619a96cfaa9f52d2b
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
    Signed-off-by: pperiyasamy <periyasamy.palanisamy@ericsson.com>

--- xci/xci-deploy.sh
+++ xci/xci-deploy.sh
@@ -38,6 +38,13 @@ source "$XCI_PATH/config/${XCI_FLAVOR}-vars"
 source $XCI_PATH/config/env-vars
 
 #-------------------------------------------------------------------------------
+# Set VM memory size to 16384 when XCI is deployed with sdn
+#-------------------------------------------------------------------------------
+if [[ $XCI_DEPLOY_OPTION == "sdn" ]]; then
+    export VM_MEMORY_SIZE=16384
+fi
+
+#-------------------------------------------------------------------------------
 # Sanitize local development environment variables
 #-------------------------------------------------------------------------------
 user_local_dev_vars=(OPNFV_RELENG_DEV_PATH OPNFV_OSA_DEV_PATH OPNFV_BIFROST_DEV_PATH)
@@ -54,6 +61,7 @@ echo "Info: Deployment parameters"
 echo "-------------------------------------------------------------------------"
 echo "xci flavor: $XCI_FLAVOR"
 echo "opnfv/releng-xci version: $OPNFV_RELENG_VERSION"
+echo "xci deployment option: $XCI_DEPLOY_OPTION"
 echo "openstack/bifrost version: $OPENSTACK_BIFROST_VERSION"
 echo "openstack/openstack-ansible version: $OPENSTACK_OSA_VERSION"
 echo "-------------------------------------------------------------------------"
