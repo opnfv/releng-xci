eea1e0f8dec23e59a0db958194ceb67e3776db9e
pperiyasamy <periyasamy.palanisamy@ericsson.com>
2017-08-14 14:52:14 +0530

    [WIP] opendaylight integration with xci
    * added ansible-opendaylight repo in a-r-r file
    * added user_variables.yml files for installing ODL and OVS (borrowed it from https://github.com/jvidalallende/releng/blob/rc5/prototypes/xci/file/mini/user_variables.yml)
    * increased VM memory footprint to accomodate ODL process
    
    Change-Id: I1355f4beb061caab7315b6f619a96cfaa9f52d2b
    Signed-off-by: Periyasamy Palanisamy <periyasamy.palanisamy@ericsson.com>
    Signed-off-by: pperiyasamy <periyasamy.palanisamy@ericsson.com>

--- xci/xci-deploy.sh
+++ xci/xci-deploy.sh
@@ -43,6 +43,13 @@ if [[ $OPNFV_SCENARIO == "os-odl-sfc" ]]; then
 fi
 
 #-------------------------------------------------------------------------------
+# Set VM memory size to 16384 when XCI is deployed with odl
+#-------------------------------------------------------------------------------
+if [[ $OPNFV_SCENARIO == "os-odl-nofeature" ]]; then
+    export VM_MEMORY_SIZE=16384
+fi
+
+#-------------------------------------------------------------------------------
 # Sanitize local development environment variables
 #-------------------------------------------------------------------------------
 user_local_dev_vars=(OPNFV_RELENG_DEV_PATH OPNFV_OSA_DEV_PATH OPNFV_BIFROST_DEV_PATH)
