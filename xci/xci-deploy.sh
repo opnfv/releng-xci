ca81c978748e72e8d0d1e30b31f5b652b7d42252
Stephen Wong <stephen.kf.wong@gmail.com>
2017-10-17 22:30:59 -0700

    xci-k8s: deploy kubernetes in XCI
    This commit introduces kubespray into XCI. Currently aio ha mini noha flavor is
    supported, and some group-vars parameters may need to be tuned to make
    kubespray more stable. k8s install currently assumes k8s install and
    OpenStack install cannot coexist, and if XCI_K8S_ENABLED is set to true,
    the xci-deploy.sh script would NOT install OpenStack. kubespray
    inventory files are currently assumed to be under the
    $OPNFV_RELENG_DEV_PATH (where the development is taken place) instead of
    upstream releng-xci repo --- that should change once this is checked in
    kubespray requirement the ansible version is 2.4.0.0
    
    For now, the following env variables NEED to be set:
    export XCI_K8S_ENABLED=true
    export OPNFV_RELENG_DEV_PATH=<your xci development directory>
    export XCI_FLAVOR=aio:
    
    note: for testing k8s deploy
    export XCI_K8S_ENABLED=true
    
    Change-Id: I3676ac246917155faec22ea4483be1acc1b35fa8
    Signed-off-by: Stephen Wong <stephen.wong1@huawei.com>
    Signed-off-by: wutianwei <wutianwei1@huawei.com>

--- xci/xci-deploy.sh
+++ xci/xci-deploy.sh
@@ -172,6 +172,34 @@ echo "Info: VM nodes are provisioned!"
 source $OPENSTACK_BIFROST_PATH/env-vars
 ironic node-list
 echo
+
+#-------------------------------------------------------------------------------
+# Deploy kubernetes
+#-------------------------------------------------------------------------------
+if [ $XCI_K8S_ENABLED == true ]; then
+
+    # TODO: destroy previous k8s environment
+    echo "Info: Configuring opnfvhost for kubespray"
+    echo "-----------------------------------------------------------------------"
+
+    cd $XCI_PATH/kubespray
+    export ANSIBLE_ROLES_PATH=$XCI_PATH/playbooks/roles
+    ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i ../playbooks/inventory configure-localhost.yml
+    cd $OPNFV_RELENG_PATH/xci/kubespray
+    ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i ../playbooks/inventory configure-opnfvhost.yml
+    if [ $XCI_FLAVOR != "aio" ]; then
+        ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i ../playbooks/inventory configure-targethosts.yml
+    fi
+
+    echo "-----------------------------------------------------------------------"
+    # for k8s 1.8, swap needs to be disabled for kubelet to run
+    ssh root@$OPNFV_HOST_IP "/sbin/swapoff -a"
+    ssh root@$OPNFV_HOST_IP "cd ${KUBESPRAY_PATH};\
+             ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i opnfv_inventory/inventory/inventory.cfg cluster.yml -b"
+    exit $?
+
+fi
+
 #-------------------------------------------------------------------------------
 # Configure localhost
 #-------------------------------------------------------------------------------
