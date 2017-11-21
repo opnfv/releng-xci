c0b5b9e379b8c7b2406e91a03869d5803c055327
Markos Chandras <mchandras@suse.de>
2017-11-21 09:35:49 +0000

    xci: Use the dib pip-and-virtualenv element
    The element will ensure that we have a reasonable set of pip
    and virtualenv packages in place as well as pick the right
    python version for the distribution. This also ensures that
    we use python2 on Xenial since dib uses python3 by default.
    
    Change-Id: I8a83af9acffbd8d6d583832956e1685100ec508a
    Signed-off-by: Markos Chandras <mchandras@suse.de>

--- xci/xci-deploy.sh
+++ xci/xci-deploy.sh
@@ -103,20 +103,22 @@ case ${OS_FAMILY,,} in
     debian)
         export DIB_OS_RELEASE="${DIB_OS_RELEASE:-xenial}"
         export DIB_OS_ELEMENT="${DIB_OS_ELEMENT:-ubuntu-minimal}"
+        # Explicitly use python2 on ubuntu
+        export DIB_PYTHON_VERSION=2
         export DIB_OS_PACKAGES="${DIB_OS_PACKAGES:-vlan,vim,less,bridge-utils,language-pack-en,iputils-ping,rsyslog,curl,iptables}"
-        export EXTRA_DIB_ELEMENTS="${EXTRA_DIB_ELEMENTS:-openssh-server}"
+        export EXTRA_DIB_ELEMENTS="${EXTRA_DIB_ELEMENTS:-openssh-server pip-and-virtualenv}"
         ;;
     redhat)
         export DIB_OS_RELEASE="${DIB_OS_RELEASE:-7}"
         export DIB_OS_ELEMENT="${DIB_OS_ELEMENT:-centos-minimal}"
         export DIB_OS_PACKAGES="${DIB_OS_PACKAGES:-vim,less,bridge-utils,iputils,rsyslog,curl,iptables}"
-        export EXTRA_DIB_ELEMENTS="${EXTRA_DIB_ELEMENTS:-openssh-server}"
+        export EXTRA_DIB_ELEMENTS="${EXTRA_DIB_ELEMENTS:-openssh-server pip-and-virtualenv}"
         ;;
     suse)
         export DIB_OS_RELEASE="${DIB_OS_RELEASE:-42.3}"
         export DIB_OS_ELEMENT="${DIB_OS_ELEMENT:-opensuse-minimal}"
         export DIB_OS_PACKAGES="${DIB_OS_PACKAGES:-vim,less,bridge-utils,iputils,rsyslog,curl,iptables}"
-        export EXTRA_DIB_ELEMENTS="${EXTRA_DIB_ELEMETS:-openssh-server}"
+        export EXTRA_DIB_ELEMENTS="${EXTRA_DIB_ELEMETS:-openssh-server pip-and-virtualenv}"
         ;;
 esac
 
