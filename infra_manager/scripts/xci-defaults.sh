a1fd50ce2b2c661924471792f9fd24d66d8693d9
Blaisonneau David <david.blaisonneau@orange.com>
2017-12-15 16:28:24 +0100

    stabilization - structure
    This patch provide the main structure for the infra manager that
    will deploy the needed servers to receive XCI VIM.
    
    All the deployments options are based on OPNFV PDF/IDF config files
    and as less as possible environment variables.
    
    For now the code is out of the main XCI folder, to allow testing
    and avoid unwanted pertubations. This will progressivly be move to
    the main structure.
    
    Change-Id: Ia759497d16743b0d685f3013c15c30a61038552b
    Signed-off-by: Blaisonneau David <david.blaisonneau@orange.com>

---
+++ infra_manager/scripts/xci-defaults.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export XCI_ANSIBLE_PIP_VERSION=${XCI_ANSIBLE_PIP_VERSION:-2.4.2.0}
+export XCI_ANSIBLE_VERBOSE=${XCI_ANSIBLE_VERBOSE:--vvv}
+
+##
+# Import config from yaml files
+##
+export XCI_ROOT=$(cat ${XCI_RUN_ROOT}/var/opnfv.yml | yq -r .xci_root)
+export POD_NAME=$(cat ${XCI_RUN_ROOT}/var/idf.yml | yq -r .idf.xci.pod_name)
+export OPNFV_USER=$(cat ${XCI_RUN_ROOT}/var/opnfv.yml | yq -r .opnfv_user)
+
+##
+# Ensure local bin folder is in the PATH
+##
+if [[ -z $(echo ${PATH} | grep "${HOME}/.local/bin")  ]]; then
+    export PATH="${HOME}/.local/bin:${PATH}"
+    export PATH="${XCI_PATH}/${POD_NAME}/bin:$PATH"
+fi
