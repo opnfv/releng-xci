ad2ab234ef7e8292b2d9a05b87db0f94745991f6
Blaisonneau David <david.blaisonneau@orange.com>
2017-12-15 16:47:14 +0100

    stabilization - XCI integration
    this patch push the proposed infra manager inside the xci-deploy script for
    debian OS family, as it is the only supported for now. New OS family will
    be supported soon
    
    Change-Id: Ib5b1cb8c8b195a11d018e5a94d9455deac90666c
    Signed-off-by: Blaisonneau David <david.blaisonneau@orange.com>

--- xci/xci-deploy.sh
+++ xci/xci-deploy.sh
@@ -94,33 +94,36 @@ echo "openstack/openstack-ansible version: $OPENSTACK_OSA_VERSION"
 echo "OPNFV scenario: $OPNFV_SCENARIO"
 echo "-------------------------------------------------------------------------"
 
-#-------------------------------------------------------------------------------
-# Install ansible on localhost
-#-------------------------------------------------------------------------------
-echo "Info: Installing Ansible from pip"
-echo "-------------------------------------------------------------------------"
-source file/install-ansible.sh
-echo "-------------------------------------------------------------------------"
-
 # Make the VMs match the host. If we need to make this configurable
 # then this logic has to be moved outside this file
+source /etc/os-release || source /usr/lib/os-release
+OS_FAMILY=${OS_FAMILY:-$ID}
 case ${OS_FAMILY,,} in
     # These should ideally match the CI jobs
-    debian)
-        export DIB_OS_RELEASE="${DIB_OS_RELEASE:-xenial}"
-        export DIB_OS_ELEMENT="${DIB_OS_ELEMENT:-ubuntu-minimal}"
-        export DIB_OS_PACKAGES="${DIB_OS_PACKAGES:-vlan,vim,less,bridge-utils,language-pack-en,iputils-ping,rsyslog,curl,iptables}"
+    ubuntu|debian)
+        export OS_FAMILY="Debian"
+        # Set ansible version to 2.4
+        # (not compatible with 2.3 - import_tasks - let see if i need to work
+        # on downgrade to 2.3
+        XCI_ANSIBLE_PIP_VERSION=2.4.2.0
+        $XCI_PATH/infra_manager/servers-prepare.sh
+        $XCI_PATH/infra_manager/nodes-deploy.sh
+        # Ensure vars used by infra_manager did not erase traditionnal vars
+        source $XCI_PATH/xci/config/env-vars
         ;;
-    redhat)
+    rhel|fedora|centos)
+        OS_FAMILY="RedHat"
         export DIB_OS_RELEASE="${DIB_OS_RELEASE:-7}"
         export DIB_OS_ELEMENT="${DIB_OS_ELEMENT:-centos-minimal}"
         export DIB_OS_PACKAGES="${DIB_OS_PACKAGES:-vim,less,bridge-utils,iputils,rsyslog,curl,iptables}"
         ;;
-    suse)
+    *suse)
+        export OS_FAMILY="Suse"
         export DIB_OS_RELEASE="${DIB_OS_RELEASE:-42.3}"
         export DIB_OS_ELEMENT="${DIB_OS_ELEMENT:-opensuse-minimal}"
         export DIB_OS_PACKAGES="${DIB_OS_PACKAGES:-vim,less,bridge-utils,iputils,rsyslog,curl,iptables}"
         ;;
+    *) echo "ERROR: unsupported OS '$OS_FAMILY'"; exit 1;;
 esac
 
 # There is no CentOS support at all
@@ -131,6 +134,14 @@ if [[ $OS_FAMILY == RedHat ]]; then
     echo ""
     exit 1
 fi
+# David_Orange - moved after case to erase the ansible used by infra_manager
+#-------------------------------------------------------------------------------
+# Install ansible on localhost
+#-------------------------------------------------------------------------------
+echo "Info: Installing Ansible from pip"
+echo "-------------------------------------------------------------------------"
+source file/install-ansible.sh
+echo "-------------------------------------------------------------------------"
 
 # Clone OPNFV scenario repositories
 #-------------------------------------------------------------------------------
@@ -161,18 +172,87 @@ fi
 # - destroys VMs, removes ironic db, leases, logs
 # - creates and provisions VMs for the chosen flavor
 #-------------------------------------------------------------------------------
-echo "Info: Starting provisining VM nodes using openstack/bifrost"
-echo "-------------------------------------------------------------------------"
-# We are using sudo so we need to make sure that env_reset is not present
-sudo sed -i "s/^Defaults.*env_reset/#&/" /etc/sudoers
-cd $XCI_PATH/bifrost/
-sudo -E bash ./scripts/destroy-env.sh
-cd $XCI_PLAYBOOKS
-ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i inventory provision-vm-nodes.yml
-cd ${XCI_CACHE}/repos/bifrost
-bash ./scripts/bifrost-provision.sh
+case ${OS_FAMILY,,} in
+    # These should ideally match the CI jobs
+    RedHat|Suse)
+      echo "Info: Starting provisining VM nodes using openstack/bifrost"
+      echo "-------------------------------------------------------------------------"
+      # We are using sudo so we need to make sure that env_reset is not present
+      sudo sed -i "s/^Defaults.*env_reset/#&/" /etc/sudoers
+      cd $XCI_PATH/bifrost/
+      sudo -E bash ./scripts/destroy-env.sh
+      cd $XCI_PLAYBOOKS
+      ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i inventory provision-vm-nodes.yml
+      cd ${XCI_CACHE}/repos/bifrost
+      bash ./scripts/bifrost-provision.sh
+      echo "-----------------------------------------------------------------------"
+      echo "Info: VM nodes are provisioned!"
+      ;;
+esac
+#-------------------------------------------------------------------------------
+# Configure localhost
+#-------------------------------------------------------------------------------
+# This playbook
+# - removes directories that were created by the previous xci run
+# - clones opnfv/releng-xci repository
+# - creates log directory
+# - copies flavor files such as playbook, inventory, and var file
+#-------------------------------------------------------------------------------
+
+echo "Info: Configuring localhost for openstack-ansible"
 echo "-----------------------------------------------------------------------"
-echo "Info: VM nodes are provisioned!"
+cd $XCI_PLAYBOOKS
+ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i inventory configure-localhost.yml
+echo "-----------------------------------------------------------------------"
+echo "Info: Configured localhost host for openstack-ansible"
+
+#-------------------------------------------------------------------------------
+# Configure openstack-ansible deployment host, opnfv
+#-------------------------------------------------------------------------------
+# This playbook
+# - removes directories that were created by the previous xci run
+# - clones opnfv/releng-xci and openstack/openstack-ansible repositories
+# - configures network
+# - generates/prepares ssh keys
+# - bootstraps ansible
+# - copies flavor files to be used by openstack-ansible
+#-------------------------------------------------------------------------------
+echo "Info: Configuring opnfv deployment host for openstack-ansible"
+echo "-----------------------------------------------------------------------"
+cd $XCI_PLAYBOOKS
+ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i ${XCI_FLAVOR_ANSIBLE_FILE_PATH}/inventory \
+    configure-opnfvhost.yml
+echo "-----------------------------------------------------------------------"
+echo "Info: Configured opnfv deployment host for openstack-ansible"
+
+#-------------------------------------------------------------------------------
+# Configure target hosts for openstack-ansible
+#-------------------------------------------------------------------------------
+# This playbook is only run for the all flavors except aio since aio is configured
+# by an upstream script.
+
+# This playbook
+# - adds public keys to target hosts
+# - configures network
+# - configures nfs
+#-------------------------------------------------------------------------------
+if [[ $XCI_FLAVOR != "aio" ]]; then
+    echo "Info: Configuring target hosts for openstack-ansible"
+    echo "-----------------------------------------------------------------------"
+    cd $XCI_PLAYBOOKS
+    ansible-playbook ${XCI_ANSIBLE_VERBOSITY} -i ${XCI_FLAVOR_ANSIBLE_FILE_PATH}/inventory \
+        configure-targethosts.yml
+    echo "-----------------------------------------------------------------------"
+    echo "Info: Configured target hosts"
+fi
+
+#-------------------------------------------------------------------------------
+# Set up target hosts for openstack-ansible
+#-------------------------------------------------------------------------------
+# This is openstack-ansible playbook. Check upstream documentation for details.
+#-------------------------------------------------------------------------------
+echo "Info: Setting up target hosts for openstack-ansible"
+>>>>>>> 2f89acd... stabilization - XCI integration
 echo "-----------------------------------------------------------------------"
 
 # Deploy OpenStack on the selected NFVI
