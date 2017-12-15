6084c570faa757dd1c52cf01980fe824ced64d01
Blaisonneau David <david.blaisonneau@orange.com>
2017-12-15 16:47:14 +0100

    XCI integration
    This patch copy the original xci-deploy.sh and change it to run
    infra_manager for VM and BM deployer.
    
    Infra manager is only compatible with Ubuntu OS
    
    Change-Id: Ib5b1cb8c8b195a11d018e5a94d9455deac90666c
    Signed-off-by: Blaisonneau David <david.blaisonneau@orange.com>

---
+++ prototypes/infra_manager/xci-deploy.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+submit_bug_report() {
+    cd ${XCI_PATH}
+    echo ""
+    echo "-------------------------------------------------------------------------"
+    echo "Oh nooooo! The XCI deployment failed miserably :-("
+    echo ""
+    echo "If you need help, please choose one of the following options"
+    echo "* #opnfv-pharos @ freenode network"
+    echo "* opnfv-tech-discuss mailing list (https://lists.opnfv.org/mailman/listinfo/opnfv-tech-discuss)"
+    echo "  - Please prefix the subject with [XCI]"
+    echo "* https://jira.opnfv.org (Release Engineering project)"
+    echo ""
+    echo "Do not forget to submit the following information on your bug report:"
+    echo ""
+    git diff --quiet && echo "releng-xci tree status: clean" || echo "releng-xci tree status: local modifications"
+    echo "opnfv/releng-xci version: $(git rev-parse HEAD)"
+    echo "openstack/bifrost version: $OPENSTACK_BIFROST_VERSION"
+    echo "openstack/openstack-ansible version: $OPENSTACK_OSA_VERSION"
+    echo "xci flavor: $XCI_FLAVOR"
+    echo "xci installer: $INSTALLER_TYPE"
+    echo "xci scenario: $DEPLOY_SCENARIO"
+    echo "Environment variables:"
+    env | grep --color=never '\(OPNFV\|XCI\|INSTALLER_TYPE\|OPENSTACK\|SCENARIO\)'
+    echo "-------------------------------------------------------------------------"
+}
+
+#-------------------------------------------------------------------------------
+# This script should not be run as root
+#-------------------------------------------------------------------------------
+if [[ $(whoami) == "root" ]]; then
+    echo "WARNING: This script should not be run as root!"
+    echo "Elevated privileges are aquired automatically when necessary"
+    echo "Waiting 10s to give you a chance to stop the script (Ctrl-C)"
+    for x in $(seq 10 -1 1); do echo -n "$x..."; sleep 1; done
+fi
+
+#-------------------------------------------------------------------------------
+# Set environment variables
+#-------------------------------------------------------------------------------
+# The order of sourcing the variable files is significant so please do not
+# change it or things might stop working.
+# - user-vars: variables that can be configured or overriden by user.
+# - pinned-versions: versions to checkout. These can be overriden if you want to
+#   use different/more recent versions of the tools but you might end up using
+#   something that is not verified by OPNFV XCI.
+# - flavor-vars: settings for VM nodes for the chosen flavor.
+# - env-vars: variables for the xci itself and you should not need to change or
+#   override any of them.
+#-------------------------------------------------------------------------------
+# find where are we
+export XCI_PATH="$(git rev-parse --show-toplevel)"
+# source user vars
+source $XCI_PATH/xci/config/user-vars
+# source pinned versions
+source $XCI_PATH/xci/config/pinned-versions
+# source flavor configuration
+source "$XCI_PATH/xci/config/${XCI_FLAVOR}-vars"
+# source installer configuration
+source "$XCI_PATH/xci/installer/${INSTALLER_TYPE}/env" &>/dev/null || true
+# source xci configuration
+source $XCI_PATH/xci/config/env-vars
+# Make sure we pass XCI_PATH everywhere
+export XCI_ANSIBLE_PARAMS+=" -e XCI_PATH=${XCI_PATH}"
+
+if [[ -z $(echo $PATH | grep "$HOME/.local/bin")  ]]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+#-------------------------------------------------------------------------------
+# Sanitize local development environment variables
+#-------------------------------------------------------------------------------
+user_local_dev_vars=(OPENSTACK_OSA_DEV_PATH OPENSTACK_BIFROST_DEV_PATH)
+for local_user_var in ${user_local_dev_vars[@]}; do
+    [[ -n ${!local_user_var:-} ]] && export $local_user_var=${!local_user_var%/}/
+done
+unset user_local_dev_vars local_user_var
+
+# register our handler
+trap submit_bug_report ERR
+
+#-------------------------------------------------------------------------------
+# Log info to console
+#-------------------------------------------------------------------------------
+echo "Info: Starting XCI Deployment"
+echo "Info: Deployment parameters"
+echo "-------------------------------------------------------------------------"
+echo "OPNFV scenario: $DEPLOY_SCENARIO"
+echo "xci flavor: $XCI_FLAVOR"
+echo "xci installer: $INSTALLER_TYPE"
+echo "opnfv/releng-xci version: $(git rev-parse HEAD)"
+echo "openstack/bifrost version: $OPENSTACK_BIFROST_VERSION"
+[[ "$INSTALLER_TYPE" == "osa" ]] && echo "openstack/openstack-ansible version: $OPENSTACK_OSA_VERSION"
+[[ "$INSTALLER_TYPE" == "kubespray" ]] && echo "kubespray version: $KUBESPRAY_VERSION"
+echo "-------------------------------------------------------------------------"
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# Install ansible on localhost
+echo "Info: Installing Ansible from pip"
+echo "-------------------------------------------------------------------------"
+bash ${XCI_PATH}/xci/files/install-ansible.sh
+echo "-------------------------------------------------------------------------"
+
+# Clone OPNFV scenario repositories
+#-------------------------------------------------------------------------------
+# This playbook
+# - removes existing scenario roles
+# - clones OPNFV scenario roles based on the xci/opnfv-scenario-requirements.yml file
+#-------------------------------------------------------------------------------
+echo "Info: Cloning OPNFV scenario repositories"
+echo "-------------------------------------------------------------------------"
+cd $XCI_PATH/xci/playbooks
+ansible-playbook ${XCI_ANSIBLE_PARAMS} -i "localhost," get-opnfv-scenario-requirements.yml
+echo "-------------------------------------------------------------------------"
+
+#-------------------------------------------------------------------------------
+# Get scenario variables overrides
+#-------------------------------------------------------------------------------
+source $(find $XCI_SCENARIOS_CACHE/${DEPLOY_SCENARIO} -name xci_overrides) &>/dev/null || :
+
+#-------------------------------------------------------------------------------
+# Start provisioning nodes
+#-------------------------------------------------------------------------------
+
+# We are using sudo so we need to make sure that env_reset is not present
+sudo sed -i "s/^Defaults.*env_reset/#&/" /etc/sudoers
+
+#-----------------------------------------------------------------------
+# Start provisioning baremetal nodes using infra_manger proto
+#-----------------------------------------------------------------------
+cd ${XCI_PATH}/prototypes/infra_manager
+unset XCI_ANSIBLE_PIP_VERSION
+./servers-prepare.sh
+./nodes-deploy.sh
+# Ensure vars used by infra_manager did not erase traditionnal vars
+source $XCI_PATH/xci/config/env-vars
+bash $XCI_PATH/xci/files/install-ansible.sh
+
+# Deploy OpenStack on the selected installer
+echo "Info: Deploying '${INSTALLER_TYPE}' installer"
+echo "-----------------------------------------------------------------------"
+source ${XCI_PATH}/xci/installer/${INSTALLER_TYPE}/deploy.sh
+
+# Deployment time
+xci_deploy_time=$SECONDS
+echo "-------------------------------------------------------------------------------------------------------------"
+echo "Info: xci_deploy.sh deployment took $(($xci_deploy_time / 60)) minutes and $(($xci_deploy_time % 60)) seconds"
+echo "-------------------------------------------------------------------------------------------------------------"
+
+# vim: set ts=4 sw=4 expandtab:
