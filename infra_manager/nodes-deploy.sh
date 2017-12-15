6b572f4905893bad1fc416078ba1d6d235ad6461
Blaisonneau David <david.blaisonneau@orange.com>
2017-12-15 16:42:02 +0100

    Add a script for servers deployment
    this patch add a script to install bifrost on the opnfv host
    'as documented' upstream, deploy servers and prepare the servers
    to host the VIM. Steps are:
      - prepare opnfv_host
      - prepare and sync bifrost sources and config on opnfv_host
      - remotely run bifrost install
      - run post_install modifications on opnfv_hosts
      - remotely run bifrost enroll then deploy
      - wait for servers
      - prepare deployed nodes
    
    Change-Id: I470229d1e5990bebcd97f62838c0b5aecdbd6148
    Signed-off-by: Blaisonneau David <david.blaisonneau@orange.com>

---
+++ infra_manager/nodes-deploy.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 Orange and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+XCI_RUN_SCRIPT=${0}
+XCI_RUN_ROOT=$(dirname $(readlink -f ${XCI_RUN_SCRIPT}))
+source ${XCI_RUN_ROOT}/scripts/xci-rc.sh
+
+# register our handler
+trap submit_bug_report ERR
+
+#-------------------------------------------------------------------------------
+# This script should not be run as root
+#-------------------------------------------------------------------------------
+no_root_needed
+
+#-------------------------------------------------------------------------------
+# Install deps
+#-------------------------------------------------------------------------------
+step_banner "Install parsing deps"
+source ${XCI_RUN_ROOT}/scripts/install-deps.sh
+source ${XCI_RUN_ROOT}/scripts/xci-defaults.sh
+
+#-------------------------------------------------------------------------------
+# Install ansible
+#-------------------------------------------------------------------------------
+step_banner "Install ansible (${XCI_ANSIBLE_PIP_VERSION})"
+source ${XCI_RUN_ROOT}/scripts/install-ansible.sh
+step_banner "Install deps for ansible plugins"
+sudo pip install netaddr
+# remove pyopenssl has it failed on CENGEN ubuntu - to be checked
+yes|sudo pip uninstall pyopenssl||true
+
+#-------------------------------------------------------------------------------
+# Prepare and install Bifrost (using official doc way)
+#  - prepare Bifrost source and config
+#  - run env-setup
+#  - run official install.yml
+#-------------------------------------------------------------------------------
+step_banner "Prepare and configure Bifrost"
+ansible-playbook ${XCI_ANSIBLE_VERBOSE} \
+  -i ${XCI_ROOT}/${POD_NAME}/etc/opnfv_hosts_inventory.yml \
+  ${XCI_RUN_ROOT}/opnfv-opnfv_host-prepare.yml
+ansible-playbook ${XCI_ANSIBLE_VERBOSE} \
+  -i ${XCI_ROOT}/${POD_NAME}/etc/opnfv_hosts_inventory.yml \
+  -t prepare \
+  -t sync \
+  ${XCI_RUN_ROOT}/opnfv-nodes-deploy.yml
+# get bifrost server ip
+export XCI_BIF_IP=$(cat ${XCI_ROOT}/${POD_NAME}/etc/opnfv_hosts_inventory.yml |\
+                    yq -r .all.hosts.opnfv_host.ansible_host)
+ssh ${OPNFV_USER}@${XCI_BIF_IP} \
+  sudo bifrost-ansible \
+    ${XCI_ANSIBLE_VERBOSE} \
+    -i inventory/target \
+    -e staging_drivers_include=true \
+    install.yaml
+
+#-------------------------------------------------------------------------------
+# Enroll and Deploy nodes (using official doc way)
+#  - run official enroll.yml
+#  - run official deploy.yml
+#-------------------------------------------------------------------------------
+step_banner "Bifrost post install customization"
+ansible-playbook \
+  -i ${XCI_ROOT}/${POD_NAME}/etc/opnfv_hosts_inventory.yml \
+  -t post_install \
+  ${XCI_RUN_ROOT}/opnfv-nodes-deploy.yml
+
+step_banner "Enroll nodes"
+ssh ${OPNFV_USER}@${XCI_BIF_IP} \
+  sudo bifrost-ansible \
+    ${XCI_ANSIBLE_VERBOSE} \
+    -i inventory/bifrost_inventory.py \
+    enroll-dynamic.yaml
+
+# As VBMC or libvirt does not seems to like bifrost multiple deploy
+# we send them sequentialy
+step_banner "Deploy nodes"
+for invt in $(ls ${XCI_ROOT}/${POD_NAME}/etc/bifrost/bifrost_inventory_*  |\
+              sed -r 's/^.+\///' | sed -r 's/\.yml//'); do
+  ssh ${OPNFV_USER}@${XCI_BIF_IP} "\
+    export INVENTORY_NAME=${invt}; \
+    sudo -E bifrost-ansible \
+      ${XCI_ANSIBLE_VERBOSE} \
+      -i inventory/bifrost_inventory.py \
+      deploy-dynamic.yaml"
+done
+
+#-------------------------------------------------------------------------------
+# Wait for servers
+#-------------------------------------------------------------------------------
+step_banner "Wait for servers to be deployed"
+ansible all -m wait_for_connection \
+  -a 'timeout=900 delay=30' \
+  -i ${XCI_ROOT}/${POD_NAME}/etc/ansible/vim_inventory
+
+#-------------------------------------------------------------------------------
+# Sanitize servers
+#-------------------------------------------------------------------------------
+step_banner "Sanitize servers"
+ansible-playbook ${XCI_ANSIBLE_VERBOSE} \
+  -i ${XCI_ROOT}/${POD_NAME}/etc/ansible/vim_inventory \
+  ${XCI_RUN_ROOT}/opnfv-nodes-prepare.yml
+
+#-------------------------------------------------------------------------------
+# Set the network on servers
+#-------------------------------------------------------------------------------
+step_banner "Set the network on servers"
+ansible-playbook ${XCI_ANSIBLE_VERBOSE} \
+  -i ${XCI_ROOT}/${POD_NAME}/etc/ansible/vim_inventory \
+  ${XCI_RUN_ROOT}/opnfv-nodes-network.yml
+
+#-------------------------------------------------------------------------------
+# Job done
+#-------------------------------------------------------------------------------
+step_banner "Servers deployed"
