3ecff12edd8440efd362a2dede0e8f53c0837e51
Markos Chandras <mchandras@suse.de>
2018-03-19 14:15:00 +0000

    xci: Install the ARA callback plugin
    The ARA Ansible plugin can be used to visualize the entire XCI run so
    lets install it by default and generate a report at the end of the
    execution.
    
    Change-Id: I66c3230b371001c110e81755c5f0dad4b1756606
    Link: https://github.com/openstack/ara
    Signed-off-by: Markos Chandras <mchandras@suse.de>

--- xci/files/install-lib.sh
+++ xci/files/install-lib.sh
@@ -14,6 +14,8 @@
 function install_ansible() {
   set -eu
 
+  local uc="https://raw.githubusercontent.com/openstack/requirements/$(awk '/requirements_git_install_branch:/ {print $2}' $XCI_PATH/xci/installer/osa/files/openstack_services.yml)/upper-constraints.txt"
+
   declare -A PKG_MAP
 
   # workaround: for latest bindep to work, it needs to use en_US local
@@ -163,7 +165,23 @@ function install_ansible() {
 
   PIP=$(which pip)
   echo "Using pip: $(${PIP} --version)"
-  ${PIP} -q install --user --upgrade virtualenv pip setuptools ansible==$XCI_ANSIBLE_PIP_VERSION
+  ${PIP} -q install --user --upgrade -c $uc ara virtualenv pip setuptools ansible==$XCI_ANSIBLE_PIP_VERSION
+
+  ara_location=$(${PYTHON} -c "import os,ara; print(os.path.dirname(ara.__file__))")
+  export ANSIBLE_CALLBACK_PLUGINS="/etc/ansible/roles/plugins/callback:${ara_location}/plugins/callbacks"
+}
+
+
+generate_ara() {
+  # Create the ARA log directory and store the sqlite source database
+  mkdir -p ${LOG_PATH}/ara/
+  rsync -a "${HOME}/.ara/ansible.sqlite" "${LOG_PATH}/ara/"
+
+  ara generate html "${LOG_PATH}/ara" || true
+  pushd ${LOG_PATH} &>/dev/null
+  tar cvf ara-report.tar ara/
+  mv ara-report.tar ara/
+  popd &> /dev/null
 }
 
 # vim: set ts=4 sw=4 expandtab:
