d5ade85d170844a8af1254a9ec55c237a0f95b02
Manuel Buil <mbuil@suse.com>
2018-05-04 23:11:35 +0200

    [WIP] Use PDF to set up the XCI VMs
    This patch is doing the first work item of the spec:
    
    https://github.com/opnfv/releng-xci/blob/master/docs/specs/infra_manager.rst
    
    It creates the required VMs by XCI to afterwards deploy the VIM. It does that
    by reading the pdf provided by the user. Please read the README for more
    information. Important assumptions (covered in README):
    
    - It is currently assumed that the OS for the VM will be installed in the first
    disk of the node described by the pdf
    
    - It is assumed that the opnfv VM characteristics are not described in the pdf
    but in a similar document called opnfv_vm.yml
    
    - All references to csv from bifrost-create-vm-nodes were removed
    
    Apart from that, to ease the review, here are some points:
    * The opnfv VM is created first and then the rest. All use the playbook:
    create_vm.yml
    
    * The create_vm.yml starts with a block which is only executed if the amount
    of required nodes (defined by the flavor) is not yet reached
    
    Change-Id: I46a85284e4ce7df21cbf66f66619b35f74251e68
    Signed-off-by: Manuel Buil <mbuil@suse.com>
    Signed-off-by: Markos Chandras <mchandras@suse.de>

--- xci/infra/bifrost/infra-provision.sh
+++ xci/infra/bifrost/infra-provision.sh
@@ -9,11 +9,41 @@
 # - creates and provisions VMs for the chosen flavor
 #-------------------------------------------------------------------------------
 BIFROST_ROOT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
+export XCI_DISTRO=${XCI_DISTRO:-$(source /etc/os-release &>/dev/null || source /usr/lib/os-release &>/dev/null; echo ${ID,,})}
+
+cd /home/opnfv/releng-xci/prototypes/playbooks/
+
+ansible-playbook ${XCI_ANSIBLE_PARAMS} \
+        -i "localhost," \
+        -e num_nodes=${NUM_NODES} \
+        -e vm_domain_type=${VM_DOMAIN_TYPE} \
+        -e baremetal_json_file=/tmp/baremetal.json \
+        -e xci_distro=${XCI_DISTRO} \
+        xci-create-vms.yaml
+
+echo "SLEEPING..."
+sleep 10
+
+sudo chmod 600 /home/opnfv/releng-xci/xci/scripts/vm/id_rsa_for_dib
+echo "Dropping a minimal .ssh/config file"
+cat > $HOME/.ssh/xci-vm-config<<EOF
+Host *
+StrictHostKeyChecking no
+ServerAliveInterval 60
+ServerAliveCountMax 5
+IdentityFile /home/opnfv/releng-xci/xci/scripts/vm/id_rsa_for_dib
+
+Host xci_vm_opnfv
+Hostname 192.168.122.2
+User root
+TCPKeepAlive yes
+StrictHostKeyChecking no
+EOF
 
 echo "Info: Starting provisining VM nodes using openstack/bifrost"
 echo "-------------------------------------------------------------------------"
 cd $BIFROST_ROOT_DIR/playbooks/
-ansible-playbook ${XCI_ANSIBLE_PARAMS} -i "localhost," bootstrap-bifrost.yml
+ansible-playbook ${XCI_ANSIBLE_PARAMS} -i /home/opnfv/releng-xci/xci/playbooks/dynamic_inventory.py bootstrap-bifrost.yml
 cd ${XCI_CACHE}/repos/bifrost
 bash ./scripts/bifrost-provision.sh
 echo "-----------------------------------------------------------------------"
