44fef59a5716972ca85e6f68fb0b2e63da087446
Manuel Buil <mbuil@suse.com>
2018-06-15 16:54:22 +0200

    [WIP] - Baremetal support
    Steps (and potential patch split):
    
    1 - Give two interfaces to the OPNFV VM. One for pxe and another for
    mgmt (192.168.122.2). Prepare two libvirt networks and two linux
    bridges for that
    
    Change-Id: I08f5d921f2adcc924ee1e0f6d945a6c9b2a24699
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- xci/infra/bifrost/infra-provision.sh
+++ xci/infra/bifrost/infra-provision.sh
@@ -30,8 +30,7 @@ ansible-playbook ${XCI_ANSIBLE_PARAMS} \
         -e vm_domain_type=${VM_DOMAIN_TYPE} \
         -e baremetal_json_file=/tmp/baremetal.json \
         -e xci_distro=${XCI_DISTRO} \
-        ${BIFROST_ROOT_DIR}/playbooks/xci-create-virtual.yml
-
+        ${BIFROST_ROOT_DIR}/playbooks/xci-create-virtual.yml -vvv
 
 ansible-playbook ${XCI_ANSIBLE_PARAMS} \
         -i ${XCI_PATH}/xci/playbooks/dynamic_inventory.py \
