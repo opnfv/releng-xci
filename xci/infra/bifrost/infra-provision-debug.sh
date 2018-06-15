3c08b009ad5bee96b6d5e1d31350713a71316bca
Manuel Buil <mbuil@suse.com>
2018-06-15 16:54:22 +0200

    [WIP] - Baremetal support
    Steps (and potential patch split):
    
    1 - Connect the interface which will receive pxe traffic to
    the bridge. That interface is found through idf and pdf and
    its value goes into netowrk_interface_pxe
    
    2 - Create the baremetal yaml file using idf and pdf through
    the playbook baremetalhosttojson.yml
    
    3 - Use bifrost-create-dib-image to create a Fedora image.
    Ericsson pod2 only works with that one. To do so we need
    
    download_ipa=false (we don't want tinylinux image)
    create_ipa_image=true
    
    4 - Configure static ip addresses based on the mac address:
     * Generate baremetalstaticips
     * inventory_dhcp=true
     * Store baremetalstaticips in /etc/dnsmasq.d/bifrost.dhcp-hosts.d/
    
    5 - Change the network config so that it uses the physical interfaces
    
    Change-Id: I08f5d921f2adcc924ee1e0f6d945a6c9b2a24699
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- xci/infra/bifrost/infra-provision-debug.sh
+++ xci/infra/bifrost/infra-provision-debug.sh
@@ -7,42 +7,30 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 #-------------------------------------------------------------------------------
-# Start provisioning VM nodes
+# Start provisioning VM nodes (ONLY FOR DEBUG)
 #-------------------------------------------------------------------------------
-# This playbook
-# - removes directories that were created by the previous xci run
-# - clones opnfv/releng-xci and openstack/bifrost repositories
-# - combines opnfv/releng-xci and openstack/bifrost scripts/playbooks
-# - destroys VMs, removes ironic db, leases, logs
-# - creates and provisions VMs for the chosen flavor
+# This playbook assumes the OPNFV VM is already created, ironic server is started
+#  and the /httboot directory already has the deployment_image.qcow2 and the
+# ipa image. This playbook reruns the provision of hosts avoiding a lot of tasks
 #-------------------------------------------------------------------------------
+
+# DO THE FOLLOWING IN THE OPNFV VM
+# * pip install python-openstackclient
+# ADD THE FOLLOWING LINES TO openrc
+# * export OS_TOKEN=fake-token
+# * export OS_URL=http://localhost:6385/
+# EXECUTE
+# * openstack baremetal node undeploy node1
+# * openstack baremetal node undeploy node2
+
+XCI_CACHE=$HOME/releng-xci/.cache
+XCI_PATH=$HOME/releng-xci
 
 BIFROST_ROOT_DIR="$(dirname $(realpath ${BASH_SOURCE[0]}))"
 export ANSIBLE_ROLES_PATH="$HOME/.ansible/roles:/usr/share/ansible/roles:/etc/ansible/roles:${XCI_PATH}/xci/playbooks/roles:${XCI_CACHE}/repos/bifrost/playbooks/roles"
 export ANSIBLE_LIBRARY="$HOME/.ansible/plugins/modules:/usr/share/ansible/plugins/modules:${XCI_CACHE}/repos/bifrost/playbooks/library"
 
-echo "Info: Create XCI VM resources"
-echo "-------------------------------------------------------------------------"
-
-ansible-playbook ${XCI_ANSIBLE_PARAMS} \
-        -i ${XCI_PATH}/xci/playbooks/dynamic_inventory.py \
-        -e num_nodes=${NUM_NODES} \
-        -e vm_domain_type=${VM_DOMAIN_TYPE} \
-        -e baremetal_json_file=/tmp/baremetal.json \
-        -e xci_distro=${XCI_DISTRO} \
-        ${BIFROST_ROOT_DIR}/playbooks/xci-create-virtual.yml
-
-
-ansible-playbook ${XCI_ANSIBLE_PARAMS} \
-        --private-key=${XCI_PATH}/xci/scripts/vm/id_rsa_for_dib \
-        --user=devuser \
-        -i ${XCI_PATH}/xci/playbooks/dynamic_inventory.py \
-        ${BIFROST_ROOT_DIR}/playbooks/xci-prepare-virtual.yml
-
 source ${XCI_CACHE}/repos/bifrost/scripts/bifrost-env.sh
-
-# This is hardcoded to delegate to localhost but we really need to delegate to opnfv instead.
-sed -i "/delegate_to:/d" ${XCI_CACHE}/repos/bifrost/playbooks/roles/bifrost-deploy-nodes-dynamic/tasks/main.yml
 
 ansible-playbook ${XCI_ANSIBLE_PARAMS} \
     --user=devuser -vvv \
@@ -52,7 +40,7 @@ ansible-playbook ${XCI_ANSIBLE_PARAMS} \
     -e testing_user=root \
     -e test_vm_num_nodes=${NUM_NODES} \
     -e test_vm_cpu='host-model' \
-    -e inventory_dhcp=true \
+    -e inventory_dhcp=false \
     -e inventory_dhcp_static_ip=false \
     -e enable_inspector=true \
     -e inspect_nodes=true \
@@ -72,7 +60,7 @@ ansible-playbook ${XCI_ANSIBLE_PARAMS} \
     -e use_prebuilt_images=${BIFROST_USE_PREBUILT_IMAGES:-false} \
     -e xci_distro=${XCI_DISTRO} \
     -e ironic_url="http://192.168.122.2:6385/" \
-    ${BIFROST_ROOT_DIR}/playbooks/opnfv-virtual.yml
+    ${BIFROST_ROOT_DIR}/playbooks/opnfv-virtual-debug.yml
 
 echo "-----------------------------------------------------------------------"
 echo "Info: VM nodes are provisioned!"
