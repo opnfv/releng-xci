e0a7514775eab1ed2daf33ef4a20a0eb3842bc86
Manuel Buil <mbuil@suse.com>
2018-08-09 16:29:24 +0200

    [Baremetal] Modify interaction with bifrost roles
    Change some of the values when doing baremetal:
    
    1 - We want to configure DHCP mapping between ip and mac when doing
    baremetal
    2 - Don't download the standard IPA image bu build one with Fedora
    (only one that works in ericsson-pod2) when doing baremetal
    3 - Wait for the blade to complete its booting. Its ssh port becomes
    available with the IPA image but we need to wait until the required
    distro gets installed. When not doing baremetal, this is fine as the
    VMs boot very fast but for baremetal we might have a race condition
    4 - Remove one call to "bifrost-create-dib-image" which was doing
    nothing
    
    Change-Id: I5536517209ff7f46ec034554d29566707778e397
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- xci/infra/bifrost/scripts/bifrost-env.sh
+++ xci/infra/bifrost/scripts/bifrost-env.sh
@@ -32,4 +32,14 @@ export BIFROST_INVENTORY_SOURCE=/tmp/baremetal.json
 
 grep -o vendor.* opnfv_vm_pdf.yml | grep -q libvirt && export BAREMETAL=true || export BAREMETAL=false
 
+if [ "${BAREMETAL}" = true ]; then
+    export BIFROST_INVENTORY_DHCP=true
+    export BIFROST_DOWNLOAD_IPA=false
+    export BIFROST_CREATE_IPA=true
+else
+    export BIFROST_INVENTORY_DHCP=false
+    export BIFROST_DOWNLOAD_IPA=true
+    export BIFROST_CREATE_IPA=false
+fi
+
 pip install -q --upgrade -r "${XCI_CACHE}/repos/bifrost/requirements.txt"
