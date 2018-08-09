a7fb4612f4d6bc37a5b3ddae347f11d518d9015d
Manuel Buil <mbuil@suse.com>
2018-08-09 15:53:40 +0200

    [baremetal] idf for opnfv_vm and update the pdf
    Physical hardware PODs provide a pdf and a idf to describe hardware and other
    information (e.g. what is the purpose for each interface). To reuse the
    same come for opnfv vm, we need an idf for the opnfv vm.
    
    As we add an idf, we should update dynamic_inventory too
    
    The pdf of opnfv vm has four interfaces but we only need two for mgmt and
    admin interfaces
    
    We add the physical interfaces that are used for mgmt and admin in the
    deployment_host vars (they are fetched from idf)
    
    As we are replacing the opnfv_vm.yml file, we remove it
    
    Change-Id: I930728474631fc214e4a9adc8581e0c16d230176
    Signed-off-by: Manuel Buil <mbuil@suse.com>

--- xci/playbooks/dynamic_inventory.py
+++ xci/playbooks/dynamic_inventory.py
@@ -76,6 +76,8 @@ class XCIInventory(object):
     def read_pdf_idf(self):
         pdf_file = os.environ['PDF']
         idf_file = os.environ['IDF']
+        opnfv_file = os.path.dirname(os.path.realpath(__file__)) + "/../var/opnfv_vm_pdf.yml"
+        opnfv_idf_file = os.path.dirname(os.path.realpath(__file__)) + "/../var/opnfv_vm_idf.yml"
         nodes = []
         host_networks = {}
 
@@ -92,6 +94,21 @@ class XCIInventory(object):
             except yaml.YAMLError as e:
                 print(e)
                 sys.exit(1)
+
+        with open(opnfv_file) as f:
+            try:
+                opnfv_pdf = yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                print(e)
+                sys.exit(1)
+
+        with open(opnfv_idf_file) as f:
+            try:
+                opnfv_idf = yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                print(e)
+                sys.exit(1)
+
 
         valid_host = (host for host in idf['xci']['installers'][self.installer]['nodes_roles'] \
                       if host in idf['xci']['flavors'][self.flavor] \
@@ -119,9 +136,32 @@ class XCIInventory(object):
                 if 'dns' in ndata.keys():
                     host_networks[hostname][network]['dns'] = str(ndata['dns'])
 
+                # Get also vlan and mac_address from pdf
+                host_networks[hostname][network]['mac_address'] = str(pdf_host_info['interfaces'][int(network_interface_num)]['mac_address'])
+                host_networks[hostname][network]['vlan'] = str(pdf_host_info['interfaces'][int(network_interface_num)]['vlan'])
+
+            # Get also vlan and mac_address from opnfv_pdf
+            mgmt_idf_index = int(opnfv_idf['opnfv_vm_idf']['net_config']['mgmt']['interface'])
+            opnfv_mgmt = opnfv_pdf['opnfv_vm_pdf']['interfaces'][mgmt_idf_index]
+            admin_idf_index = int(opnfv_idf['opnfv_vm_idf']['net_config']['admin']['interface'])
+            opnfv_public = opnfv_pdf['opnfv_vm_pdf']['interfaces'][admin_idf_index]
+            self.opnfv_networks['opnfv']['mgmt']['mac_address'] = str(opnfv_mgmt['mac_address'])
+            self.opnfv_networks['opnfv']['mgmt']['vlan'] = str(opnfv_mgmt['vlan'])
+            self.opnfv_networks['opnfv']['public']['mac_address'] = str(opnfv_public['mac_address'])
+            self.opnfv_networks['opnfv']['public']['vlan'] = str(opnfv_public['vlan'])
+
+            # Add the interfaces from idf
+
+
             host_networks.update(self.opnfv_networks)
 
             self.add_groupvar('all', 'host_info', host_networks)
+
+        if 'deployment_host_interfaces' in idf['xci']['installers'][self.installer]['network']:
+            mgmt_idf_index = int(opnfv_idf['opnfv_vm_idf']['net_config']['mgmt']['interface'])
+            admin_idf_index = int(opnfv_idf['opnfv_vm_idf']['net_config']['admin']['interface'])
+            self.add_hostvar('deployment_host', 'network_interface_admin', idf['xci']['installers'][self.installer]['network']['deployment_host_interfaces'][admin_idf_index])
+            self.add_hostvar('deployment_host', 'network_interface_mgmt', idf['xci']['installers'][self.installer]['network']['deployment_host_interfaces'][mgmt_idf_index])
 
         # Now add the additional groups
         for parent in idf['xci']['installers'][self.installer]['groups'].keys():
