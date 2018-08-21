e1b1f28d3ad3224e518faa50a04e04bde51c4829
Fatih Degirmenci <fdegir@gmail.com>
2018-08-21 14:55:28 +0200

    Print OpenStack login information at the end
    The info is logged to the console before post-deployment tasks and
    this makes it seeing the deployment status and login info inconvenient.
    This change moves the logging to the end, after post-deployment.
    
    Change-Id: Ic7e232a0b8343e666f54087fbcc09481ab59ca90
    Signed-off-by: Fatih Degirmenci <fdegir@gmail.com>

--- xci/installer/osa/deploy.sh
+++ xci/installer/osa/deploy.sh
@@ -160,6 +160,22 @@ echo
 echo "Info: OpenStack installation is successfully completed!"
 
 #-------------------------------------------------------------------------------
+# Execute post-installation tasks
+#-------------------------------------------------------------------------------
+# Playbook post.yml is used in order to execute any post-deployment tasks that
+# are required for the scenario under test.
+#-------------------------------------------------------------------------------
+echo "-----------------------------------------------------------------------"
+echo "Info: Running post-deployment scenario role"
+echo "-----------------------------------------------------------------------"
+cd $OSA_XCI_PLAYBOOKS
+ansible-playbook ${XCI_ANSIBLE_PARAMS} -i ${XCI_PLAYBOOKS}/dynamic_inventory.py \
+    post-deployment.yml
+echo "-----------------------------------------------------------------------"
+echo
+echo "Info: Post-deployment scenario role execution done"
+
+#-------------------------------------------------------------------------------
 # - Getting OpenStack login information
 #-------------------------------------------------------------------------------
 echo "Info: Openstack login details"
@@ -180,19 +196,4 @@ echo "Info: Admin username -  ${USERNAME##*=}"
 echo "Info: Admin password - ${PASSWORD##*=}"
 echo "Info: It is recommended to change the default password."
 
-#-------------------------------------------------------------------------------
-# Execute post-installation tasks
-#-------------------------------------------------------------------------------
-# Playbook post.yml is used in order to execute any post-deployment tasks that
-# are required for the scenario under test.
-#-------------------------------------------------------------------------------
-echo "-----------------------------------------------------------------------"
-echo "Info: Running post-deployment scenario role"
-echo "-----------------------------------------------------------------------"
-cd $OSA_XCI_PLAYBOOKS
-ansible-playbook ${XCI_ANSIBLE_PARAMS} -i ${XCI_PLAYBOOKS}/dynamic_inventory.py \
-    post-deployment.yml
-echo "-----------------------------------------------------------------------"
-echo
-echo "Info: Post-deployment scenario role execution done"
 # vim: set ts=4 sw=4 expandtab:
