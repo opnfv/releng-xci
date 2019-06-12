a8ece480b4214a8a1866690782902a10fe382f4c
Victor Morales <victor.morales@intel.com>
2019-06-12 11:27:19 -0700

    Enable mitogen for k8s deployment
    Mitogen helps to speed up the ansible deployment time. This feature
    has been implemented in Kubespray can can be used since v2.8.0
    
    Change-Id: Id9b6f81f392eebd984a3f9f3dae87137304b9bb7
    Signed-off-by: Victor Morales <victor.morales@intel.com>

--- xci/installer/kubespray/deploy.sh
+++ xci/installer/kubespray/deploy.sh
@@ -74,7 +74,7 @@ echo "Info: Using kubespray to deploy the kubernetes cluster"
 echo "-----------------------------------------------------------------------"
 ssh root@$OPNFV_HOST_IP "set -o pipefail; export XCI_FLAVOR=$XCI_FLAVOR; export INSTALLER_TYPE=$INSTALLER_TYPE; \
         export IDF=/root/releng-xci/xci/var/idf.yml; export PDF=/root/releng-xci/xci/var/pdf.yml; \
-        cd releng-xci/.cache/repos/kubespray/; ansible-playbook \
+        cd releng-xci/.cache/repos/kubespray/; make mitogen; ansible-playbook \
         -i opnfv_inventory/dynamic_inventory.py cluster.yml -b | tee setup-kubernetes.log"
 scp root@$OPNFV_HOST_IP:~/releng-xci/.cache/repos/kubespray/setup-kubernetes.log \
         $LOG_PATH/setup-kubernetes.log
