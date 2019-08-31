6459934028496af1e544889576c6244519768ee1
Fatih Degirmenci <fdegir@gmail.com>
2019-08-31 18:50:05 +0200

    Refactor k8s scenario implementation
    This change updates the scenario logic for basic k8s
    scenarios that enables the use of various network plugins.
    
    The change simply stops copying k8s-cluster.yml over but instead
    operates on the existing/original k8s-cluster.yml and updates
    kube_network_plugin in place.
    
    This makes it easier to maintain the scenarios and more importantly
    unblocks us from bumping kubespray and kubernetes versions.
    
    Please note that opnfv-scenario-requirements.yml contains version and
    the refspec of the corresponding change in releng-xci-scenarios repo
    so this change must be verified properly and if it passes CI, it
    needs to be amended, updating k8-calico-nofeature scenario's version
    to master and submitted. And then the change in releng-xci-scenarios
    can properly be verified.
    
    https://gerrit.opnfv.org/gerrit/#/c/releng-xci-scenarios/+/68418/
    
    installer-type:kubespray
    deploy-scenario:k8-calico-nofeature
    
    JIRA: XCI-20
    
    Change-Id: I1f851d20f5f7bc256199303581b05659091fddf9
    Signed-off-by: Fatih Degirmenci <fdegir@gmail.com>

--- xci/installer/kubespray/deploy.sh
+++ xci/installer/kubespray/deploy.sh
@@ -33,6 +33,20 @@ echo "-----------------------------------------------------------------------"
 echo "Info: Configured localhost for kubespray"
 
 #-------------------------------------------------------------------------------
+# Configure installer
+#-------------------------------------------------------------------------------
+# TODO: summarize what this playbook does
+#-------------------------------------------------------------------------------
+
+echo "Info: Configuring kubespray installer"
+echo "-----------------------------------------------------------------------"
+cd $K8_XCI_PLAYBOOKS
+ansible-playbook ${XCI_ANSIBLE_PARAMS} \
+        -i ${XCI_PLAYBOOKS}/dynamic_inventory.py configure-installer.yml
+echo "-----------------------------------------------------------------------"
+echo "Info: Configured kubespray installer"
+
+#-------------------------------------------------------------------------------
 # Configure deployment host, opnfv
 #-------------------------------------------------------------------------------
 # This playbook
@@ -75,7 +89,7 @@ echo "-----------------------------------------------------------------------"
 ssh root@$OPNFV_HOST_IP "set -o pipefail; export XCI_FLAVOR=$XCI_FLAVOR; export INSTALLER_TYPE=$INSTALLER_TYPE; \
         export IDF=/root/releng-xci/xci/var/idf.yml; export PDF=/root/releng-xci/xci/var/pdf.yml; \
         cd releng-xci/.cache/repos/kubespray/; ansible-playbook \
-        -i opnfv_inventory/dynamic_inventory.py cluster.yml -b | tee setup-kubernetes.log"
+        -i inventory/opnfv/dynamic_inventory.py cluster.yml -b | tee setup-kubernetes.log"
 scp root@$OPNFV_HOST_IP:~/releng-xci/.cache/repos/kubespray/setup-kubernetes.log \
         $LOG_PATH/setup-kubernetes.log
 
@@ -88,12 +102,9 @@ echo "-----------------------------------------------------------------------"
 echo "Info: Kubernetes installation is successfully completed!"
 echo "-----------------------------------------------------------------------"
 
-# Configure the kubernetes authentication in opnfv host. In future releases
-# kubectl is no longer an artifact so we should not fail if it's not available.
-# This needs to be removed in the future
-ssh root@$OPNFV_HOST_IP "mkdir -p ~/.kube/;\
-         cp -f ~/admin.conf ~/.kube/config; \
-         cp -f ~/kubectl /usr/local/bin || true"
+# TODO: configure artifacts_dir in k8s-cluster.yml so admin.conf gets created in ansible_env.HOME
+ssh root@$OPNFV_HOST_IP "mkdir -p ~/.kube/; \
+    cp -f ~/releng-xci/.cache/repos/kubespray/inventory/opnfv/artifacts/admin.conf ~/.kube/config"
 
 #-------------------------------------------------------------------------------
 # Execute post-installation tasks
